683e80f23933dded209dcf76c362e2aa2c188429
Matthew Dunavant <madunavant@gmail.com>
2019-04-11 10:04:10 -0400

    Formatted HTML, fixed imgSrc bug

--- frontend/brewery-finder/src/views/ViewBeer.vue
+++ frontend/brewery-finder/src/views/ViewBeer.vue
@@ -1,66 +1,55 @@
 <template>
-    <section>
-        
-            <img src="#" alt="Beer Image">
-            <p>Name</p>
-        
-        <p>ABV</p>
-        <p>Style</p>
-        <p>Favorite</p>
-        <the-map></the-map>
+  <section>
+    <img src="#" alt="Beer Image">
+    <p>Name</p>
 
-        <table>
-            <thead>
-                <tr><th>Name</th>
-                <th>Address</th>
-                <th>Happy Hours</th>
-                <th>Bar or Brewery</th>
-                </tr>
+    <p>ABV</p>
+    <p>Style</p>
+    <p>Favorite</p>
+    <the-map></the-map>
 
-            </thead>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Address</th>
+          <th>Happy Hours</th>
+          <th>Bar or Brewery</th>
+        </tr>
+      </thead>
 
-            <tbody>
-                <tr v-for="brewery in breweries" v-bind:key="brewery.id"><th>Name</th>
-                <th>Address</th>
-                <th>Happy Hours</th>
-                <th>Bar or Brewery</th>
-                </tr>
-
-            </tbody>
-        </table>
-    </section>
+      <tbody>
+        <brewery-info v-for="brewery in breweries" v-bind:key="brewery.id" v-bind:brewery="brewery"></brewery-info>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script>
-import TheMap from '@/components/TheMap.vue'
+import TheMap from "@/components/TheMap.vue";
 export default {
-    components:{
-        TheMap,
-    },
-    data() {
-        return {
-            breweries:[]
-        }
-    },
-    computed: {
-
-    },
-    methods: {
-
-    },
-    created(){
-        const id=this.$route.params.id;
-        fetch(`${process.env.vue_app_remote_api}/beer/${id}`, {
-            method:"get"
-
-        }).then(response=>response.json())
-        .then(json=>{
-            this.breweries=json;
-        })
-    }
-}
+  components: {
+    TheMap
+  },
+  data() {
+    return {
+      breweries: []
+    };
+  },
+  computed: {},
+  methods: {},
+  created() {
+    const id = this.$route.params.id;
+    fetch(`${process.env.vue_app_remote_api}/beer/${id}`, {
+      method: "get"
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.breweries = json;
+      });
+  }
+};
 </script>
 
 <style>
-
 </style>
