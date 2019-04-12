e92a07095c34460f5e6a35301102938379a039ca
Brandon House <bhouse33@gmail.com>
2019-04-12 09:49:31 -0400

    Started search brewery vue.

---
+++ frontend/brewery-finder/src/views/SearchBrewery.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            breweries : []
+        }
+    },
+
+    methods: {
+        loadBreweries() {
+            fetch (`${process.env.VUE_APP_REMOTE_API}/brewery?zip=44145`,{
+                method:"GET",
+
+            }).then(response => response.json())
+            .then(json => this.breweries = json)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
