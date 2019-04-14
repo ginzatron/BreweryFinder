dfa449aa0531c212475f5de919ce26caaa856430
ginzatron <43377218+ginzatron@users.noreply.github.com>
2019-04-14 12:06:47 -0500

    gross

--- frontend/brewery-finder/src/views/Home.vue
+++ frontend/brewery-finder/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <section class="home">
-    <the-map></the-map>
+    
+    <the-map>
+      
+    </the-map>
+    
   </section>
 </template>
 
@@ -15,3 +19,25 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+section {
+    background-image: url('~@/assets/vats.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    min-height: 1325px;
+}
+
+div.transbox {
+  margin: 30px;
+  background-color: #ffffff;
+  border: 1px solid black;
+  opacity: 0.6;
+}
+
+div.transbox p {
+  font-weight: bold;
+  color: #000000;
+}
+</style>
