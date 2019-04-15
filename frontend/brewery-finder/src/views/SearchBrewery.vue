679a4a9480a6fef92d105f165e7f0180d9d67345
Noah Ginsburg <njginsburg@gmail.com>
2019-04-15 10:58:03 -0400

    form css

--- frontend/brewery-finder/src/views/SearchBrewery.vue
+++ frontend/brewery-finder/src/views/SearchBrewery.vue
@@ -2,14 +2,41 @@
   <section>
     <div>
       <form v-on:submit.prevent="loadBreweries">
+        <div>
         <label>Name</label>
         <input type="text" placeholder="Brewery Name" v-model.trim="formData.name">
+        </div>
+        <div>
         <label>Zip Code</label>
         <input type="text" placeholder="Zip Code" v-model.trim="formData.zipCode">
+        </div>
+        <div>
         <label>Radius</label>
         <input type="text" placeholder="Radius in miles" v-model.trim="formData.range">
+        </div>
+        <div>
         <label>Happy Hour</label>
-        <input type="text" placeholder="Happy Hour" v-model.trim="formData.happyHour">
+        <select v-model.trim="formData.happyHour">
+          <option value=""></option>
+          <option value="">11:00 am</option>
+          <option value="">11:30 am</option>
+          <option value="">12:00 pm</option>
+          <option value="">12:30 pm</option>
+          <option value="">1:00 pm</option>
+          <option value="">1:30 pm</option>
+          <option value="">2:00 pm</option>
+          <option value="">2:30 pm</option>
+          <option value="">3:00 pm</option>
+          <option value="">3:30 pm</option>
+          <option value="">4:00 pm</option>
+          <option value="">4:30 pm</option>
+          <option value="">5:00 pm</option>
+          <option value="">5:30 pm</option>
+          <option value="">6:00 pm</option>
+          <option value="">6:30 pm</option>
+          <option value="">7:00 pm</option>
+        </select>
+        </div>
         <button type="submit">Submit</button>
       </form>
     </div>
@@ -46,14 +73,35 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
   form {
+    display: flex;
+    flex-direction: column;
     margin-top: 20px;
     margin-bottom: 20px;
+    align-items: space-between;
+    width: 420px;
+    margin-left: 40%;
+    border: 1px solid black;
+    padding: 8px;
+    border-radius: 10px;
   }
 
   label{
     color: black;
     font-weight: bold;
+  }
+  input, select {
+    width: 300px;
+    margin-bottom:12px;
+  }
+  form div {
+    display: flex;
+    justify-content: space-between;
+  }
+  button {
+    margin-top: 12px;
+    width: 99px;
+    margin-left: 40%;
   }
 </style>
