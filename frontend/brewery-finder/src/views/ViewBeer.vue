eabc1c061ea721aefc82d94d5968a30db0ee637d
Ken Bohac <bohac91@gmail.com>
2019-04-16 13:25:53 -0400

    added images and database text and viewbeer template

--- frontend/brewery-finder/src/views/ViewBeer.vue
+++ frontend/brewery-finder/src/views/ViewBeer.vue
@@ -1,28 +1,26 @@
 <template>
   <section>
-    <img src="#" alt="Beer Image">
-    <p>Name</p>
-
-    <p>ABV</p>
-    <p>Style</p>
-    <p>Favorite</p>
-    <the-map></the-map>
-
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Address</th>
-          <th>Happy Hours</th>
-          <th>Bar or Brewery</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <brewery-info v-for="brewery in breweries" v-bind:key="brewery.id" v-bind:brewery="brewery"></brewery-info>
-      </tbody>
-    </table>
+    
+    <div class="beer-deet">
+      <div class="beer-details-top">
+        <img id="beer-img" v-bind:src="`${beer.imgSrc}`" :alt="`${beer.name}` + 'image'">
+        <div id="beer-info">
+          <div id="beer-name">
+            <p>{{beer.name}}</p>
+          </div>
+         
+          <div id=beer-description>
+          <p>{{beer.description}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h2 class="card">Beer Available at {{brewery[0].name}}</h2>
+    <div class="available-breweries">
+      <beer-info v-bind:key="beer.id" v-bind:beer="beer" v-for="beer in brewery[0].beersAvailable"></beer-info>
+    </div>
   </section>
+
 </template>
 
 <script>
@@ -34,6 +32,7 @@ export default {
   data() {
     return {
       breweries: []
+      
     };
   },
   computed: {},
@@ -52,4 +51,54 @@ export default {
 </script>
 
 <style>
+.beer-details-top {
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-template-areas: "brewery-image brewery-info";
+}
+
+h2 {
+  text-decoration: underline;
+  font-size: 3rem;
+  display: inline-block;
+}
+
+#beer-img {
+  max-width: 85%;
+  height: 350px;
+  padding: 40px;
+}
+
+#beer-img {
+  grid-area: brewery-image;
+}
+
+#beer-name{
+  font-size: 1.5rem;
+  color: #490000;
+}
+
+#beer-info {
+  grid-area: brewery-info;
+  margin: auto 0;
+  text-align: left;
+  justify-self: left;
+  font-weight: bold;
+  margin-right: 30px;
+  border: 3px solid var(--burgundy);
+  border-radius: 10px;
+  background-color: rgba(167, 176, 173, 0.9);
+  padding: 15px;
+}
+
+#beer-description{
+  margin-top: 30px;
+  font-weight: bold;
+}
+
+div.available-breweries {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
 </style>
