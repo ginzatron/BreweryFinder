bcce86318a9a7d326f7f698c7595018a577f6a31
Noah Ginsburg <njginsburg@gmail.com>
2019-04-16 12:21:24 -0400

    formatting

--- frontend/brewery-finder/src/views/SearchBrewery.vue
+++ frontend/brewery-finder/src/views/SearchBrewery.vue
@@ -1,54 +1,54 @@
 <template>
   <section>
-    <form class="card" v-on:submit.prevent="loadBreweries">
-      <div>
-        <label>Name</label>
-        <input type="text" placeholder="Brewery Name" v-model.trim="formData.name">
-      </div>
-      <div>
-        <label>Zip Code</label>
-        <input type="text" placeholder="Zip Code" v-model.trim="formData.zipCode">
-      </div>
-      <div>
-        <label>Radius</label>
-        <input type="text" placeholder="Radius in miles" v-model.trim="formData.range">
-      </div>
-      <div>
-        <label>Happy Hour</label>
-        <select v-model.trim="formData.happyHour">
-          <option value></option>
-          <option value="11:00">11:00 am</option>
-          <option value="11:30">11:30 am</option>
-          <option value="12:00">12:00 pm</option>
-          <option value="12:30">12:30 pm</option>
-          <option value="13:00">1:00 pm</option>
-          <option value="13:30">1:30 pm</option>
-          <option value="14:00">2:00 pm</option>
-          <option value="14:30">2:30 pm</option>
-          <option value="15:00">3:00 pm</option>
-          <option value="15:30">3:30 pm</option>
-          <option value="16:00">4:00 pm</option>
-          <option value="16:30">4:30 pm</option>
-          <option value="17:00">5:00 pm</option>
-          <option value="17:30">5:30 pm</option>
-          <option value="18:00">6:00 pm</option>
-          <option value="18:30">6:30 pm</option>
-          <option value="19:00">7:00 pm</option>
-        </select>
-      </div>
-      <div>
-        <label>Beer Name</label>
-        <input type="text" placeholder="Beer Name" v-model.trim="formData.beerName">
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-    <div class="map-results">
-       <div class="results">
+    <div class="form-results">
+      <form class="card" v-on:submit.prevent="loadBreweries">
+        <div>
+          <label>Name</label>
+          <input type="text" placeholder="Brewery Name" v-model.trim="formData.name">
+        </div>
+        <div>
+          <label>Zip Code</label>
+          <input type="text" placeholder="Zip Code" v-model.trim="formData.zipCode">
+        </div>
+        <div>
+          <label>Radius</label>
+          <input type="text" placeholder="Radius in miles" v-model.trim="formData.range">
+        </div>
+        <div>
+          <label>Happy Hour</label>
+          <select v-model.trim="formData.happyHour">
+            <option value></option>
+            <option value="11:00">11:00 am</option>
+            <option value="11:30">11:30 am</option>
+            <option value="12:00">12:00 pm</option>
+            <option value="12:30">12:30 pm</option>
+            <option value="13:00">1:00 pm</option>
+            <option value="13:30">1:30 pm</option>
+            <option value="14:00">2:00 pm</option>
+            <option value="14:30">2:30 pm</option>
+            <option value="15:00">3:00 pm</option>
+            <option value="15:30">3:30 pm</option>
+            <option value="16:00">4:00 pm</option>
+            <option value="16:30">4:30 pm</option>
+            <option value="17:00">5:00 pm</option>
+            <option value="17:30">5:30 pm</option>
+            <option value="18:00">6:00 pm</option>
+            <option value="18:30">6:30 pm</option>
+            <option value="19:00">7:00 pm</option>
+          </select>
+        </div>
+        <div>
+          <label>Beer Name</label>
+          <input type="text" placeholder="Beer Name" v-model.trim="formData.beerName">
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div class="results">
         <results v-bind:breweries="breweries"></results>
       </div>
-      <div id="map">
-        <the-map></the-map>
-      </div>
+    </div>
+    <div id="map">
+      <the-map></the-map>
     </div>
   </section>
 </template>
@@ -110,34 +110,39 @@ export default {
 form {
   display: flex;
   flex-direction: column;
-  margin-bottom: 20px;
   align-items: space-between;
-  width: 35%;
+  width: 100%;
   margin-top: 10px;
+  margin-bottom: 10px;
 }
 
-.map-results {
+.results {
+  width: 100%;
+}
+
+.form-results {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-evenly;
 }
 
 #map {
   height: 400px;
   width: 800px;
+  margin-top:10px;
+  margin-bottom:30px;
 }
 
-section div {
+section {
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-direction: row;
+  justify-content: space-evenly;
 }
 label {
   color: black;
   font-weight: bold;
 }
-input,
-select {
+input,select {
   width: 300px;
   margin-bottom: 12px;
 }
